9419b0437f5d698a4900e3dc1da80111f20ee751
jagaraj10 <37337265+jagaraj10@users.noreply.github.com>
2018-03-21 10:57:03 +0530

    Create biggest of three.py

---
+++ beginner level/biggest of three.py
@@ -0,0 +1,9 @@
+a=int(input("enter no 1:")
+b=int(input("enter no 2:")
+c=int(input("enter no 3:")
+if(a>b)and(a>c):
+    print(a ,"is big")
+elif(b>c):
+    print(b ,"is big")
+else:
+    print(c, "is big")
